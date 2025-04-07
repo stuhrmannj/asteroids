52e878f84ed7bfc6b24a987c226c6981a92ba06c
stuhrmannj <stuhrmannj@hotmail.com>
2025-04-06 20:16:27 -0400

    asteroid ship creation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -11,12 +12,26 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()  #clock object helps control game's frame rate
+    dt = 0 # delta time variable stores how much time passes between frames
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #instantiate a Player object
+
     while True: # This loop will run indefinitely
-        for event in pygame.event.get():
+        for event in pygame.event.get(): # this allows the close window button to work
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")  # This paints the entire screen black for this frame
+        for entity in drawable: # This creates the objects of the game
+            entity.draw(screen) 
+        updatable.update(dt)  #This updates the game with inputs (like moving and steering the ship)
         pygame.display.flip()  # This updates the window with the new frame
+        dt = clock.tick(60) / 1000 #sets the game loop to 60 frames per second
 
-if __name__ == "__main__":
+if __name__ == "__main__": #I'm not sure what this does...
     main()
